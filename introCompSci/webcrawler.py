d641beae7b40f0e2de2e48bb978c9bf059b347bb
Francososa <franky.sosa@gmail.com>
2015-12-18 23:13:08 -0600

    Add Lesson 1 final quiz to webcrawler.py

--- introCompSci/webcrawler.py
+++ introCompSci/webcrawler.py
@@ -1,1 +1,30 @@
 print "Hello world!"
+
+# Your code should print http://udacity.com
+# Make sure that if page were changed to
+
+# page = '<a href="http://udacity.com">Hello world</a>'
+
+# that your code still assigns the same value to the variable 'url',
+# and therefore still prints the same thing.
+
+# page = contents of a web page
+
+#Lesson 1: my version
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+
+start_link = page.find('<a href=')
+second_paren = start_link + 9
+end_link = page.find('"', second_paren)
+url = page[start_link + 9:end_link]
+print url
+
+#Lesson 1: class version
+page = ('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+
+start_link = page.find('<a href=')
+start_quote = page.find('"', start_link)
+end_quote = page.find('"', start_quote + 1)
+url = page[start_quote + 1: end_quote]
