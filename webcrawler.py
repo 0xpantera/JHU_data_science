3c9d02fd1bf10a79293b2c0c018152736ac16260
Francososa <franky.sosa@gmail.com>
2016-01-24 19:15:28 -0600

    Add webcrawler project

---
+++ webcrawler.py
@@ -0,0 +1,42 @@
+print "Hello world!"
+
+# Your code should print http://udacity.com
+# Make sure that if page were changed to
+
+# page = '<a href="http://udacity.com">Hello world</a>'
+
+# that your code still assigns the same value to the variable 'url',
+# and therefore still prints the same thing.
+
+# page = contents of a web page
+
+#Lesson 1: my version
+page =('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+
+start_link = page.find('<a href=')
+second_paren = start_link + 9
+end_link = page.find('"', second_paren)
+url = page[start_link + 9:end_link]
+print url
+
+#Lesson 1: class version
+page = ('<div id="top_bin"><div id="top_content" class="width960">'
+'<div class="udacity float-left"><a href="http://udacity.com">')
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1: end_quote]
+    return url, end_quote
+
+def print_all_links(page):
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            print url
+        else:
+            break
